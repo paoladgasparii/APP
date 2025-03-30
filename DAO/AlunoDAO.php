b238b9df96b5a1538ce0f701106083b639b4017e
paola <paola@LAPTOP-JBURO0B7>
2025-03-30 11:14:37 -0300

    avanço

---
+++ DAO/AlunoDAO.php
@@ -0,0 +1,65 @@
+<?php
+
+    namespace App\DAO;
+    use App\Model\Aluno;
+
+    final class AlunoDAO extends DAO
+    {
+        public function __construct();
+        {
+            parent::__construct();
+        }
+
+        public function save (Aluno $model) : Aluno
+        {
+            return($model->Id == null) ? $this->insert($model) :
+            $this->update($model);
+        }
+
+        public function insert(Aluno $model): Aluno
+        {
+            $sql = "INSERT INTO aluno (nome, ra, curso) VALUES (?, ?, ?) ";
+
+            $stmt = parent::$conexao->prepare($sql);
+
+            $stmt->bindValue(1, $model->Nome);
+            $stmt->bindValue(2, $model->RA);
+            $stmt->bindValue(3, $model->Curso);
+            $stmt->execute();
+            $model->Id = parent::$conexao->lastInsertId();
+
+            return $model;
+        }
+
+        public function update(Aluno $model): Aluno
+        {
+            $sql = "UPDATE aluno SET nome?, ra?, curso? WHERE id=?";
+
+            $stmt->bindValue(1, $model->Nome);
+            $stmt->bindValue(2, $model->RA);
+            $stmt->bindValue(3, $model->Curso);
+            $stmt->bindValue(4, $model->Id);
+            $stmt->execute();
+            $model->Id = parent::$conexao->lastInsertId();
+
+            return $model;
+        }
+
+        public function selectById(int $id) : ?Aluno
+        {
+            $sql = "SELECT * FROM aluno WHERE id=?";
+
+            $stmt = parent::$conexao->prepare($sql);
+
+            $stmt->bindValue(1, $model->Nome);
+            $stmt->bindValue(2, $model->RA);
+            $stmt->bindValue(3, $model->Curso);           
+            $stmt->execute();
+
+            return $model;
+        }
+
+       
+    }
+
+?>
---
+++ Model/Aluno.php
@@ -0,0 +1,74 @@
+<?php
+
+    namespace App\Model;
+    {
+        use App\DAO\AlunoDAO;
+        use Exception;
+
+        final class Aluno extends Model
+        {
+            public ?int $id = null;
+
+            public ?string $Nome
+            {
+                set 
+                {
+                    if(strlen($value) < 3)
+                        throw new Exception("Nome deve ter no mínimo 3 caracteres.");
+
+                        $this->Nome = $value;
+                }
+
+                get => $this->Nome ??null;
+            }
+
+            public ?string $RA
+            {
+                set 
+                {
+                    if(empty($value))
+                        throw new Exception("Preencha o RA.");
+
+                        $this->RA = $value;
+                }
+
+                get => $this->RA ??null;
+            }
+
+            public ?string $Curso
+            {
+                set 
+                {
+                    if(strlen($value) < 3)
+                        throw new Exception("Curso deve ter no mínimo 3 caracteres.");
+
+                        $this->Curso = $value;
+                }
+
+                get => $this->Curso ??null;
+            }
+
+            function save (): Aluno
+            {
+                return new AlunoDAO()->save($this);
+            }
+
+            function getById(int $id): ?Aluno
+            {
+                return new AlunoDAO()->selectById($id);
+            }
+
+            function getAllRows(): array
+            {
+                $this->rows = new AlunoDAO()->select();
+
+                return $this->rows;
+            }
+
+            function delete(int $id): bool
+            {
+                return new AlunoDAO()->delete($id);
+            }
+        }
+    }
+?>
